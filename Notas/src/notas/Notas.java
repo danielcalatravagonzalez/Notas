784c93c7b9f60a7088acf205bf6cfa2ff9fdcab5
danielcalatravagonzalez <degixcala@gmail.com>
2022-02-16 17:54:38 +0100

    3.Utiliza las funciones de refactorización de código, utilizando Eclipse y Netbeans, para renombrar aquellos identificadores que consideres no tienen un nombre adecuado, especificando detalladamente como lo haces y por qué los eliges. Sube las modificaciones al repositorio de Github que has creado para el proyecto de Eclipse, siendo el comentario el enunciado de este ejercicio.

--- Notas/src/notas/Notas.java
+++ Notas/src/notas/Notas.java
@@ -18,21 +18,21 @@ public class Notas {
 		// TODO Auto-generated method stub
 
 		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-		int n = 0;
-		String c = "";
+		int notas = 0;
+		String calificacion = "";
 		System.out.print("Introduzca una nota: ");
-		n = reader.nextInt();
-		if (n >= 0 && n < 5)
-			c = "Suspenso";
-		else if (n >= 5 && n < 7)
-			c = "Aprobado";
-		else if (n >= 7 && n < 9)
-			c = "Notable";
-		else if (n >= 9 && n <= 10)
-			c = "Sobresaliente";
+		notas = reader.nextInt();
+		if (notas >= 0 && notas < 5)
+			calificacion = "Suspenso";
+		else if (notas >= 5 && notas < 7)
+			calificacion = "Aprobado";
+		else if (notas >= 7 && notas < 9)
+			calificacion = "Notable";
+		else if (notas >= 9 && notas <= 10)
+			calificacion = "Sobresaliente";
 		else
-			c = "La nota introducida no es correcta";
-		System.out.println(c);
+			calificacion = "La nota introducida no es correcta";
+		System.out.println(calificacion);
 		reader.close();
 	}
 
